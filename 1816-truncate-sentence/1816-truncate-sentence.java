5b93245fb0e994274e9be3e7db1ec0ac6b051210
Anshika Srivastava <srianshika89@gmail.com>
2026-07-30 01:38:23 +0530

    Time: 1 ms (73.1%), Space: 43.3 MB (44.84%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+
+        String[] arr=s.split(" ");
+
+        StringBuilder ans=new StringBuilder();
+
+        for(int i=0;i<k;i++){
+
+            ans.append(arr[i]);
+
+            if(i!=k-1){
+                ans.append(" ");
+            }
+
+        }
+
+        return ans.toString();
+    }
+}
